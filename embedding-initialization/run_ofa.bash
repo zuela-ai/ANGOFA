6bdd0116541b30a9a7ba75111fc53cb931c01fcd
cx-olquinjica <aosiwaduo@outlook.com>
2024-03-12 09:22:30 +0800

    add:subword_embedding_initialization

---
+++ embedding-initialization/run_ofa.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+source_model_name="xlm-roberta-base"
+target_model_name="cis-lmu/glot500-base"
+source_language_set="None"
+target_language_set="None"
+keep_dim="[100,200,400,768]"
+embedding="../ColexificationNet/network_related"
+path="./stored_factorization"
+python -u ofa.py \
+    --source_model_name $source_model_name \
+    --target_model_name $target_model_name \
+    --source_language_set $source_language_set \
+    --target_language_set $target_language_set \
+    --keep_dim $keep_dim \
+    --do_save "true" \
+    --embedding_path $embedding \
+    --save_path $path
